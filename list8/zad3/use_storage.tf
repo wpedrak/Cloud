8a1c07a71abddbc365532eff21e35dce62a01978
wpedrak <woj123@gmail.com>
2019-01-06 17:03:27 +0100

    pubsub

--- list8/zad3/use_storage.tf
+++ list8/zad3/use_storage.tf
@@ -3,6 +3,8 @@ provider "google" {
   region = "europe-west4"
 }
 
+data "google_project" "project" {}
+
 resource "google_service_account" "zad8-3" {
   account_id   = "zad8-3"
   display_name = "zad8-3"
@@ -16,6 +18,24 @@ resource "google_project_iam_binding" "storage-rw" {
   ]
 }
 
+# resource "google_project_iam_binding" "pubsub" {
+#   role    = ""
+
+#   members = [
+#     "serviceAccount:${google_service_account.zad8-3.email}",
+#   ]
+# }
+
+resource "google_pubsub_topic" "task-topic" {
+  name = "topic-8-3"
+}
+
+resource "google_pubsub_subscription" "task-sub" {
+  name  = "subscription-8-3"
+  topic = "${google_pubsub_topic.task-topic.name}"
+
+  ack_deadline_seconds = 180
+}
 
 resource "google_compute_instance" "server" {
   name         = "test"
@@ -36,6 +56,8 @@ resource "google_compute_instance" "server" {
       // Ephemeral IP
     }
   }
+
+  metadata_startup_script = "export PROJECT_ID=${data.google_project.project.project_id}; export BUCKET_NAME=${google_storage_bucket.result-holder.name}; export SUBSCRIPTION_NAME=${google_pubsub_subscription.task-sub.name}; echo ${data.google_project.project.project_id} > ~/a.txt; echo ${google_storage_bucket.result-holder.name} > ~/b.txt; echo ${google_pubsub_subscription.task-sub.name} > ~/c.txt"
 
   service_account {
     email = "${google_service_account.zad8-3.email}"
