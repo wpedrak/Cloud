8e838a0e8e695e88ee12b05360a9091fff4c7b59
wpedrak <woj123@gmail.com>
2019-01-06 15:37:03 +0100

    python task for processing

--- list8/zad3/task.py
+++ list8/zad3/task.py
@@ -1,4 +1,11 @@
-from google.cloud import storage
+from google.cloud import storage, pubsub_v1
+
+import time
+
+PROJECT_ID = "tmp1"
+BUCKET_NAME = "tmp2"
+SUBSCRIPTION_NAME = "tmp3"
+
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
     storage_client = storage.Client()
@@ -9,5 +16,45 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
         source_file_name,
         destination_blob_name))
 
-        
+
+def write_result(result, result_filename):
+    NAME = result_filename
+    file = open(result_filename, 'w')
+    file.write(', '.join(result))
+    file.close()
+    upload_blob(BUCKET_NAME, result_filename, result_filename)
+
+
+def parse_message(message):
+    return [int(x) for x in split(message)]
+
+def is_prime(number):
+    return not any([number % x == 0 for x in range(2,number)])
+
+def find_primes(range_from, range_to):
+    return [x for x in range(range_from, range_to) if is_prime(x)]
+
+
+def callback(message):
+    print('Received message: {}'.format(message))
+    range_from, range_to = parse_message(message)
+    result = find_primes(range_from, range_to)
+    result_filename = "result-from-{}-to-{}.txt".format(range_from, range_to)
+    write_result(result, result_filename)
+    message.ack()
+
+
+subscriber = pubsub_v1.SubscriberClient()
+# The `subscription_path` method creates a fully qualified identifier
+# in the form `projects/{project_id}/subscriptions/{subscription_name}`
+subscription_path = subscriber.subscription_path(project_id, subscription_name)
+subscriber.subscribe(subscription_path, callback=callback)
+
+# The subscriber is non-blocking. We must keep the main thread from
+# exiting to allow it to process messages asynchronously in the background.
+print('Listening for messages on {}'.format(subscription_path))
+while True:
+    time.sleep(60)
+
+
 upload_blob("result-holder", 'tmp.txt', 'result.txt')
