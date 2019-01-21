8da1010a65ed1eb6f9e7db117a9997dc805ecbe7
Wojciech Pędrak <woj123@gmail.com>
2019-01-21 11:44:24 +0100

    better nodes - better gke

--- list10/zad3/setup/setup_kubernetes.tf
+++ list10/zad3/setup/setup_kubernetes.tf
@@ -1,12 +1,12 @@
 resource "google_container_cluster" "cluster" {
   name               = "cluster"
   zone               = "europe-west4-a"
-  initial_node_count = 3
+  initial_node_count = 4
 
   network = "projects/cloud2018-list2/global/networks/default"
 
   node_config {
-    machine_type = "n1-standard-1"
+    machine_type = "n1-highcpu-2"
     disk_size_gb = 10
     # image_type   = "COS"
   }
