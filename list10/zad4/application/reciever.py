8957c22e31446f11f1731ae8ab420f62178fd960
wpedrak <woj123@gmail.com>
2019-01-26 20:20:09 +0100

    reciever +/- for prod

--- list10/zad4/application/reciever.py
+++ list10/zad4/application/reciever.py
@@ -10,8 +10,34 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(
 channel = connection.channel()
 channel.queue_declare(queue=QUEUE_NAME)
 
+
+def parse_message(message):
+    text = message.data.decode('utf-8')
+    return [int(x) for x in text.split()]
+
+
+def is_prime(number):
+    if number < 2:
+        return False
+    return all([number % x != 0 for x in range(2, number)])
+
+
+def find_primes(range_from, range_to):
+    return [x for x in range(range_from, range_to) if is_prime(x)]
+
+
+def write_result(result):
+    print(result)
+
+
 def callback(ch, method, properties, body):
+    message = body
     print("Received:", body)
+    range_from, range_to = parse_message(message)
+    result = find_primes(range_from, range_to)
+
+    write_result(result)
+
 
 channel.basic_consume(callback,
                       queue=QUEUE_NAME,
